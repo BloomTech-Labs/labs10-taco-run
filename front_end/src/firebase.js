7ff46414cc487d76ab6ec2f7545df6ba444dfec0
marshall <marshall@marshalls-MacBook-Air.local>
2019-02-18 14:13:32 -0800

    vars update

--- front_end/src/firebase.js
+++ front_end/src/firebase.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
+require('dotenv').config()
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_UR,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID
+  apiKey: "AIzaSyCwMbSKhR9BW3xXic9KP37plKiHUWb-wuY",
+  authDomain: "mock-up-d0d2d.firebaseapp.com",
+  databaseURL: "https://mock-up-d0d2d.firebaseio.com",
+  projectId: "mock-up-d0d2d",
+  storageBucket: "mock-up-d0d2d.appspot.com",
+  messagingSenderId: "7261255904"
 };
 
+console.log(process.env.apiKey)
 
 firebase.initializeApp(config);
 export const auth = firebase.auth();
