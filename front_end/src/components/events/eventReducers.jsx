1f6e8d5f348e93380c502b0334dc74fe1b71c560
Mr. Metelus <Mdmetelus@gmail.com>
2019-02-19 14:10:36 -0500

    greated some events reducers

--- front_end/src/components/events/eventReducers.jsx
+++ front_end/src/components/events/eventReducers.jsx
@@ -1,4 +1,35 @@
-import { createReducer } from '../../app/common/util/reducerUtil';
 import { CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT } from './eventConstants';
 
 const initialState = [];
+
+
+
+export const createEvent = (state, payload) => {
+    return [...state, Object.assign({}, payload.event)]
+  }
+  
+  export const updateEvent = (state, payload) => {
+    return [
+      ...state.filter(event => event.id !== payload.event.id),
+      Object.assign({}, payload.event)
+    ]
+  }
+  
+  export const deleteEvent = (state, payload) => {
+    return [
+      ...state.filter(event => event.id !== payload.eventId)
+    ]
+  }
+  
+  export default createReducer(initialState, {
+    [CREATE_EVENT]: createEvent,
+    [UPDATE_EVENT]: updateEvent,
+    [DELETE_EVENT]: deleteEvent
+  })
+
+  createReducer = (initialState, fnMap) => {
+    return (state = initialState, {type, payload}) => {
+      const handler = fnMap[type];
+  
+      return handler ? handler(state, payload): state
+    };
