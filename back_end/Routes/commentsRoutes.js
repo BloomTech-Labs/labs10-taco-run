067dcd0b361c1ca0f0ef74d478705dd8d3dc5c02
lanners-marshall <lanners.marshall@gmail.com>
2019-02-20 13:27:26 -0800

    updated route for adding comment and updating comment count in event table

--- back_end/Routes/commentsRoutes.js
+++ back_end/Routes/commentsRoutes.js
@@ -8,15 +8,42 @@ const db = require("../config.js")
 //create a new comment
 //-------------------------------------------
 router.post('', (req, res) => {
-	const {content, date, author, event_id } = req.body;
+	const { id } = req.params;
+	const {content, date, author } = req.body;
 
-	db.insert({content, date, author, event_id }).into('comments')
-		.then(response => {
-			res.status(201).json(response)
+	//add comment to database
+	db.insert({content, date, author, event_id: id }).into('comments')
+		.then(res1 => {
+			
+			//see the current comment count for event
+			db('events')
+			.where({id})
+			.first()
+			.then(res2 => {
+
+				//take current comment count and add 1 to it
+				let num = res1.total_comments
+				num = num + 1
+
+				db('events')
+				.where({id})
+				.update({total_comments: num})
+				.then(res3 => {
+					return res.status(200).json(res2)
+				})
+				.catch(err3 => { //catch for updating event
+					console.log(error)
+					return res.status(500).json(err3)
+				})
+			})
+			.catch(err2 => { //catch for looking up event
+				console.log(error)
+				return res.status(500).json(err2)
+			})
 		})
-		.catch(error => {
-			console.log(error)
-			res.status(500).json({msg: "there was an error creating project"})
+		.catch(err1 => { //catch for inserting comment
+			console.log(err1)
+			res.status(500).json(err1)
 		})
 
 })
