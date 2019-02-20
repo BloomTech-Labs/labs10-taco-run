1d057b82a7925a886cf29d35063e35d1d4aca0ec
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-02-20 13:01:17 -0800

    Component: Initialized state object for event, and imported needed dependencies

--- front_end/src/components/events2/CreateEvent.js
+++ front_end/src/components/events2/CreateEvent.js
@@ -1,15 +1,21 @@
 import React from 'react';
-//import axios from 'axios';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { createEvent } from '../../store/actions/eventsActions';
 
 class CreateEvent extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
-			name: '',
-			date: '',
-			invite: true,
+			event: {
+				name: '',
+				date: '',	
+				location: '',
+				venue: '',
+				author: ''
+			} // --> event object				
 		};
-	}
+	} // --> constructor
 
 	componentDidMount(){
 
