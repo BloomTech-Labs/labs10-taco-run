d03bb3b304fbedf64e33e3604279d23b81aa89ae
lanners-marshall <lanners.marshall@gmail.com>
2019-02-19 20:59:05 -0800

    updated front end of form for users edit profile

--- front_end/src/components/user/UserSettings.js
+++ front_end/src/components/user/UserSettings.js
@@ -2,18 +2,34 @@ import React from 'react';
 import Nav from '../nav/Nav.js'
 import Select from 'react-select';
 import './settings.css'
-import {ProfileForm, ContainForm, Reset, Switch, SwitchTab} from './user_settings_css.js'
+import {ProfileForm, ContainForm, Reset, Switch, SwitchTab, FlexDiv, CenterDiv, Submit} from './user_settings_css.js'
 import axios from 'axios';
 import { withAlert } from 'react-alert'
 import PhoneInput from 'react-phone-number-input'
 import { isValidPhoneNumber } from 'react-phone-number-input'
 import Billing from '../billing/Billing.js'
-import StripeCheckout from "react-stripe-checkout";
 
 const options = [
   { value: 'phone', label: 'phone' },
   { value: 'email', label: 'email' },
 ];
+
+const options1 = [
+  {value: 'soft', label: 'soft'},
+  {value: 'hard', label: 'hard'}
+]
+
+const options2 = [
+  {value: 'mild', 'label': 'mild'},
+  {value: 'medium', 'label': 'medium'},
+  {value: 'spicy', 'label': 'spicy'},
+  {value: 'HOT!', 'label': 'Hot!'}
+]
+
+const options3 = [
+  {value: 'street', 'label': 'street'},
+  {value: 'gourmet', 'label': 'gourmet'}
+]
 
 class UserSettings extends React.Component {
   constructor(props){
@@ -23,7 +39,10 @@ class UserSettings extends React.Component {
       phone: '',
       bymail: true,
       byphone: false,
-      selectedOption: null,
+      selectedOption: '',
+      selectedOption1: '',
+      selectedOption2: '',
+      selectedOption3: '',
       profile: true,
       selected: ['', 'active'],
       usersName: '',
@@ -51,6 +70,21 @@ class UserSettings extends React.Component {
   handleSelect = (selectedOption) => {
     this.setState({ selectedOption });
     console.log(`Option selected:`, selectedOption);
+  }
+
+  handleSelect1 = (selectedOption1) => {
+    this.setState({ selectedOption1 });
+    console.log(`Option selected:`, selectedOption1);
+  }
+
+  handleSelect2 = (selectedOption2) => {
+    this.setState({ selectedOption2 });
+    console.log(`Option selected:`, selectedOption2);
+  }
+
+  handleSelect3 = (selectedOption3) => {
+    this.setState({ selectedOption3 });
+    console.log(`Option selected:`, selectedOption3);
   }
 
   switchToBilling = () => {
@@ -85,6 +119,7 @@ class UserSettings extends React.Component {
           (
             <div>
               <ProfileForm>
+                <Reset>Reset Pass</Reset>
                 <h2>Edit Profile</h2>
                 <input
                   type="text"
@@ -100,19 +135,52 @@ class UserSettings extends React.Component {
                   onChange={ phone => this.setState({ phone }) }
                   className="phone-input" 
                 />
-                <h3>Reminder Method</h3>
-                <Select
-                  value={this.state.selectedOption}
-                  onChange={this.handleSelect}
-                  options={options}
-                  isSearchable={true}
-                  isMulti={true}
-                  className="select"
-                />
               </ProfileForm>
-              <div>SUBMIT</div>
-              <Reset>Pass Reset Email</Reset>
-            </div>
+              <h2 className="prefs">Preferances</h2>
+              <FlexDiv>
+                <div>
+                  <h3>Reminder Method</h3>
+                  <Select
+                    value={this.state.selectedOption}
+                    onChange={this.handleSelect}
+                    options={options}
+                    isSearchable={true}
+                    isMulti={true}
+                    className="select"
+                  />
+                </div>
+                <div>
+                  <h3>Shell Type</h3>
+                  <Select
+                    value={this.state.selectedOption1}
+                    onChange={this.handleSelect1}
+                    options={options1}
+                    className="select"
+                  />
+                </div>
+              </FlexDiv>
+              <FlexDiv>
+                <div>
+                  <h3>Spiciness</h3>
+                  <Select
+                    value={this.state.selectedOption2}
+                    onChange={this.handleSelect2}
+                    options={options2}
+                    className="select"
+                  />
+                </div>
+                <div>
+                  <h3>Restaurant</h3>
+                  <Select
+                    value={this.state.selectedOption3}
+                    onChange={this.handleSelect3}
+                    options={options3}
+                    className="select"
+                  />
+                </div>
+              </FlexDiv>
+            <Submit>SUBMIT</Submit>
+          </div>
           ) 
             : 
             <Billing/>
