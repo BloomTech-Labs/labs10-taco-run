994c0ed0edffa4e5933db78d01a488a797668964
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-02-21 09:32:58 -0800

    Styling Create Event: Imported the styled components file, and reformatting the jsx to use StyledComponents module

--- front_end/src/components/events2/CreateEvent.js
+++ front_end/src/components/events2/CreateEvent.js
@@ -2,6 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import { createEvent } from "../../store/actions/eventsActions";
 
+import Nav from "../nav/Nav.js";
+import { CreateEventWrapper, FormElement } from "./create_event_css.js";
+
 class CreateEvent extends React.Component {
 	constructor(props) {
 		super(props);
@@ -50,58 +53,63 @@ class CreateEvent extends React.Component {
 
 
 	render() {
-		return (
-			<div className = "create-event-wrapper">
-				<form onSubmit = {this.handleSubmit}>
+		return (	
+			<div className = "create-event-full-wrapper">
+				<div className = "navigation-wrapper">
+						<Nav />
+				</div>
+				<CreateEventWrapper>				
+					<FormElement onSubmit = {this.handleSubmit}>
 
-					<label for = "event-name">Event Name</label>
-					<input 
-						name = "name"
-						onChange = {this.handleChange}
-						value = {this.state.event.name}
-						type = "text"	
-						id = "event-name"					
-					/>
+						<label for = "event-name">Event Name</label>
+						<input 
+							name = "name"
+							onChange = {this.handleChange}
+							value = {this.state.event.name}
+							type = "text"	
+							id = "event-name"					
+						/>
 
-					<label for = "event-date">Event Date</label>
-					<input 
-						name = "date"
-						onChange = {this.handleChange}
-						value = {this.state.event.date}
-						type = "date"	
-						id = "event-date"					
-					/>
+						<label for = "event-date">Event Date</label>
+						<input 
+							name = "date"
+							onChange = {this.handleChange}
+							value = {this.state.event.date}
+							type = "date"	
+							id = "event-date"					
+						/>
 
-					<label for = "event-location">Event Location</label>
-					<input 
-						name = "location"
-						onChange = {this.handleChange}
-						value = {this.state.event.location}
-						type = "text"	
-						id = "event-location"					
-					/>
+						<label for = "event-location">Event Location</label>
+						<input 
+							name = "location"
+							onChange = {this.handleChange}
+							value = {this.state.event.location}
+							type = "text"	
+							id = "event-location"					
+						/>
 
-					<label for = "event-venue">Venue / Building</label>
-					<input 
-						name = "venue"
-						onChange = {this.handleChange}
-						value = {this.state.event.venue}
-						type = "text"	
-						id = "event-venue"					
-					/>
+						<label for = "event-venue">Venue / Building</label>
+						<input 
+							name = "venue"
+							onChange = {this.handleChange}
+							value = {this.state.event.venue}
+							type = "text"	
+							id = "event-venue"					
+						/>
 
-					<label for = "event-author">Event Author</label>
-					<input 
-						name = "author"
-						onChange = {this.handleChange}
-						value = {this.state.event.author}
-						type = "text"	
-						id = "event-author"					
-					/>
+						<label for = "event-author">Event Author</label>
+						<input 
+							name = "author"
+							onChange = {this.handleChange}
+							value = {this.state.event.author}
+							type = "text"	
+							id = "event-author"					
+						/>
 
-					<button type = "submit">Submit</button>
+						<button type = "submit">Submit</button>
 
-				</form>
+					</FormElement>
+				</CreateEventWrapper>
 			</div>
 		)
 	} // --> render() brace
---
+++ front_end/src/components/events2/create_event_css.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const CreateEventWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #F3F7F8;
+`
+
+export const FormElement = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: lightblue;
+  padding: 0 5.6rem;
+  border-radius: 10px;
+`
