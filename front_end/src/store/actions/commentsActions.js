3a61f0883720b3cc07557414e0a60e4742857a3c
benny <oseguerabenny27@gmail.com>
2019-02-21 10:51:36 -0800

    Added: Actions/reducer for comment update

--- front_end/src/store/actions/commentsActions.js
+++ front_end/src/store/actions/commentsActions.js
@@ -3,12 +3,18 @@ import axios from "axios";
 export const COMMENTS_GET_START = "COMMENTS_GET_START";
 export const COMMENTS_GET_COMPLETE = "COMMENTS_GET_COMPLETE";
 export const COMMENTS_GET_ERROR = "COMMENTS_GET_ERROR";
+
 export const MAKE_COMMENT_START = "MAKE_COMMENT_START";
 export const MAKE_COMMENT_COMPLETE = "MAKE_COMMENT_COMPLETE";
-export const MAKE_COMMENT_ERROR = "MAKE_COMMENT_ERROR"
+export const MAKE_COMMENT_ERROR = "MAKE_COMMENT_ERROR";
+
+export const UPDATE_COMMENT_START = "UPDATE_COMMENT_START";
+export const UPDATE_COMMENT_COMPLETE = "UPDATE_COMMENT_COMPLETE";
+export const UPDATE_COMMENT_ERROR = "UPDATE_COMMENT_ERROR";
+
 export const DELETE_COMMENT_START = "DELETE_COMMENT_START";
 export const DELETE_COMMENT_COMPLETE = "DELETE_COMMENT_COMPLETE";
-export const DELETE_COMMENT_ERROR = "DELETE_COMMENT_ERROR"
+export const DELETE_COMMENT_ERROR = "DELETE_COMMENT_ERROR";
 
 export const getComments = id => {
   return dispatch => {
@@ -29,7 +35,7 @@ export const getComments = id => {
 
 export const makeComment = comment => {
   return dispatch => {
-    dispatch({type: MAKE_COMMENT_START});
+    dispatch({ type: MAKE_COMMENT_START });
     axios
       .post(`https://production-taco.herokuapp.com/comments`, comment)
       .then(res => {
@@ -37,31 +43,50 @@ export const makeComment = comment => {
         dispatch({
           type: MAKE_COMMENT_COMPLETE,
           payload: obj
-        })
+        });
       })
       .catch(err => {
-        console.log(err)
-        dispatch({type: MAKE_COMMENT_ERROR, payload: err})
-      })
-  }
-}
+        console.log(err);
+        dispatch({ type: MAKE_COMMENT_ERROR, payload: err });
+      });
+  };
+};
 
-export const deleteComment = (ids, cid) => {
+export const updateComment = (ids, cid) => {
   return dispatch => {
-    dispatch({type: DELETE_COMMENT_START })
+    dispatch({ type: UPDATE_COMMENT_START });
     axios
       .delete(`https://production-taco.herokuapp.com/comments`, ids)
       .then(res => {
-        console.log(res)
+        console.log(res);
         dispatch({
-          type: DELETE_COMMENT_COMPLETE,
+          type: UPDATE_COMMENT_COMPLETE,
           payload: res.data,
-          id: cid,
-        })
+          id: cid
+        });
       })
       .catch(err => {
-        console.log(err)
-        dispatch({type: DELETE_COMMENT_ERROR, payload: err })
+        console.log(err);
+        dispatch({ type: UPDATE_COMMENT_ERROR, payload: err });
+      });
+  };
+};
+
+export const deleteComment = changes => {
+  return dispatch => {
+    dispatch({ type: UPDATE_COMMENT_START });
+    axios
+      .put(`http://localhost:5555/comments`, { changes })
+      .then(res => {
+        console.log(res);
+        dispatch({
+          type: UPDATE_COMMENT_COMPLETE,
+          payload: res.data
+        });
       })
-  }
-}
+      .catch(err => {
+        console.log(err);
+        dispatch({ type: UPDATE_COMMENT_ERROR, payload: err });
+      });
+  };
+};
