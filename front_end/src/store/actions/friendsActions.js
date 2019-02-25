1db884ec6d3c384be57d64d34f58f59420ac09c6
benny <oseguerabenny27@gmail.com>
2019-02-25 11:22:49 -0800

    Fixed: Endpoint for user_friends to heroku

--- front_end/src/store/actions/friendsActions.js
+++ front_end/src/store/actions/friendsActions.js
@@ -8,7 +8,7 @@ export const addFriend = friend => {
   return dispatch => {
     dispatch({ type: FRIEND_ADD_START });
     axios
-      .post(`http://localhost:5555/users_friends`, friend)
+      .post(`https://production-taco.herokuapp.com/users_friends`, friend)
       .then(res => {
         let obj = JSON.parse(res.config.data);
         dispatch({
