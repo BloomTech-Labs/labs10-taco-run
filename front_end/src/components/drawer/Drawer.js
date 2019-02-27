0c76f55dad64cb04d386a64afce496ecbc27288f
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-02-26 18:32:44 -0800

    Imports: Imported the required items for redux and firebase auth

--- front_end/src/components/drawer/Drawer.js
+++ front_end/src/components/drawer/Drawer.js
@@ -1,1 +1,6 @@
 import React from 'react';
+
+// --> Base imports for redux and to use firebase auth
+import { withRouter } from "react-router-dom";
+import firebase from 'firebase';
+import {connect} from 'react-redux';
