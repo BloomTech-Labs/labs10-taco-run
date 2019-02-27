4f2321db53cffbff47c96c7763d7afb5262e5c8b
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-02-26 18:33:29 -0800

    Material-UI Imports: Imported elements from material-ui

--- front_end/src/components/drawer/Drawer.js
+++ front_end/src/components/drawer/Drawer.js
@@ -4,3 +4,16 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 import firebase from 'firebase';
 import {connect} from 'react-redux';
+
+// --> Base Imports for the elements used from @material-ui
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
