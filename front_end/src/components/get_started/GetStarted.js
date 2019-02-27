1e4d315b920ff2e93e07c383baf96030bab47078
Mr. Metelus <Mdmetelus@gmail.com>
2019-02-27 15:45:19 -0500

    button changed, text backgroundand borders added, exported with styles.

--- front_end/src/components/get_started/GetStarted.js
+++ front_end/src/components/get_started/GetStarted.js
@@ -5,6 +5,13 @@ import { MainDiv, TopHr, BottomHr } from './started_css.js'
 import { Link } from "react-router-dom";
 import { CreateLink, ContainLink } from '../events2/eventlist_css.js';
 import DrawerBar from "../drawer/Drawer";
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+// import ReactMarkdown from 'markdown-to-jsx';
+import Typography from '@material-ui/core/Typography';
+import classNames from 'classnames';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class GetStarted extends React.Component {
   constructor(){
@@ -17,14 +24,16 @@ class GetStarted extends React.Component {
   }
 
   render() {
+    const { classes, theme } = this.props;
     return (
       <div>
         <DrawerBar />
         <TopHr />
         <MainDiv>
-          <div>
+          <div className={classes.mainFeaturedPost}>
+          <div className={classes.layout}>
             <div>
-              <h2>Welcome to<br/>Lets Get Tacos</h2>
+              <h2>Welcome to<br/>Let's Get Tacos</h2>
               <h3>A free taco event site</h3>
             </div>
             <p>
@@ -33,11 +42,17 @@ class GetStarted extends React.Component {
               Create your own event too. Don't forget to share on social media! Lets eat!
             </p>
             <ContainLink>
-              <Link to="events_create"><CreateLink>Create Event</CreateLink></Link>
+              <Link to="events_create">
+                <Button variant="outlined" size="medium">
+                  Create Event
+                </Button>
+              
+              </Link>
             </ContainLink>
           </div>
+          </div>
           <div>
-            <img src={running}/>
+            <img className={classes.imgRun} src={running}/>
           </div>
         </MainDiv>
         <BottomHr/>
@@ -46,4 +61,49 @@ class GetStarted extends React.Component {
   }
 }
 
-export default GetStarted;
+const styles = theme => ({
+
+  mainFeaturedPost: {
+    backgroundColor: theme.palette.grey[300],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing.unit * 4,
+  },
+  mainFeaturedPostContent: {
+    padding: `${theme.spacing.unit * 6}px`,
+    [theme.breakpoints.up('md')]: {
+      paddingRight: 0,
+    },
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 4,
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    // [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+    //   width: 1100,
+    //   marginLeft: 'auto',
+    //   marginRight: 'auto',
+    // },
+  },
+  ingRun: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 6,
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
+    paddingTop: theme.spacing.unit * 3,
+    paddingBottom: theme.spacing.unit * 6,
+
+  },
+
+});
+
+// export default GetStarted;
+export default withStyles(styles)(GetStarted);
