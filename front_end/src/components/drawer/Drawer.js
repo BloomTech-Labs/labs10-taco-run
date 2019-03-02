3acfb14d7553ba17f64b0bb94ae26c880002b0ee
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-02 11:18:59 -0800

    Re-factor Drawer Navigation: Changed button logos and re-arranged order

--- front_end/src/components/drawer/Drawer.js
+++ front_end/src/components/drawer/Drawer.js
@@ -56,12 +56,12 @@ class DrawerBar extends React.Component {
 		this.props.history.push("/user-profile")
 	}
 
-	users = () => {
-		this.props.history.push("/users")
+	createEvent = () => {
+		this.props.history.push("/events_create")
 	}
 
-	started = () => {
-		this.props.history.push("/get-started")
+	settings = () => {
+		this.props.history.push("/user-settings")
 	}
 
 	logOut = (event) => {
@@ -126,18 +126,32 @@ class DrawerBar extends React.Component {
 
           <Divider /> {/* This is the ----- that shows up */}
           <List> {/* This is the beginning of the side-bar navigation: We map through the possible options and then.... */}
-            {['Events Dashboard', 'Profile', 'Users', 'Get Started', 'Sign Out'].map((text, index) => (  
+            {['Events Dashboard', 'Create Event', 'Profile', 'Settings', 'Sign Out'].map((text, index) => (  
               <div className = "side-menu-wrap" key = {text}> 
               {/* Here for each index (each button / item): we want to render a different icon */}
               {/* <SvgIcon><path></SvgIcon> --> This is to be able to access a huge amount of different icons from `materialdesignicons.com` */}
                 {
-                  index === 0 ? (
+                  index === 0 ? (                                                              
                     <ListItem button key = {text} onClick = {this.events}>
-                      <ListItemIcon><HomeIcon /></ListItemIcon>
-                      <ListItemText primary = {text} />
-                    </ListItem>                                      
+                    <ListItemIcon>
+                      <SvgIcon>
+                        <path fill="#000000" d="M13,3V9H21V3M13,21H21V11H13M3,21H11V15H3M3,13H11V3H3V13Z" />  
+                      </SvgIcon>  
+                    </ListItemIcon>
+                    <ListItemText primary = {text} />
+                  </ListItem>    
                   ) : 
-                  index === 1 ? (
+                  index === 1 ? (                    
+                    <ListItem button key = {text} onClick = {this.createEvent}>
+                      <ListItemIcon>
+                        <SvgIcon>
+                          <path fill="#000000" d="M9,11.5A2.5,2.5 0 0,0 11.5,9A2.5,2.5 0 0,0 9,6.5A2.5,2.5 0 0,0 6.5,9A2.5,2.5 0 0,0 9,11.5M9,2C12.86,2 16,5.13 16,9C16,14.25 9,22 9,22C9,22 2,14.25 2,9A7,7 0 0,1 9,2M15,17H18V14H20V17H23V19H20V22H18V19H15V17Z" />
+                        </SvgIcon>
+                      </ListItemIcon>
+                      <ListItemText primary = {text} />
+                    </ListItem>                              
+                  ) :
+                  index === 2 ? (
                     <ListItem button key = {text} onClick = {this.profile}>
                       <ListItemIcon>
                         <SvgIcon>
@@ -145,30 +159,19 @@ class DrawerBar extends React.Component {
                         </SvgIcon>                        
                       </ListItemIcon>
                       <ListItemText primary = {text} />
-                    </ListItem>                                      
-                  ) :
-                  index === 2 ? (
-                    <ListItem button key = {text} onClick = {this.users}>
-                      <ListItemIcon>
-                        <SvgIcon>
-                          <path fill="#000000" d="M7.5,15C8.63,15 9.82,15.26 11.09,15.77C12.35,16.29 13,16.95 13,17.77V20H2V17.77C2,16.95 2.65,16.29 3.91,15.77C5.18,15.26 6.38,15 7.5,15M13,13H22V15H13V13M13,9H22V11H13V9M13,5H22V7H13V5M7.5,8A2.5,2.5 0 0,1 10,10.5A2.5,2.5 0 0,1 7.5,13A2.5,2.5 0 0,1 5,10.5A2.5,2.5 0 0,1 7.5,8Z" />
-                        </SvgIcon>
-                      </ListItemIcon>
-                      <ListItemText primary = {text} />
-                    </ListItem>                                      
+                    </ListItem>                                   
                   ) :
                   index === 3 ? (
-                    <ListItem button key = {text} onClick = {this.started}>
+                    <ListItem button key = {text} onClick = {this.settings}>
                       <ListItemIcon>
                         <SvgIcon>
-                          <path fill="#000000" d="M7.5,15C8.63,15 9.82,15.26 11.09,15.77C12.35,16.29 13,16.95 13,17.77V20H2V17.77C2,16.95 2.65,16.29 3.91,15.77C5.18,15.26 6.38,15 7.5,15M13,13H22V15H13V13M13,9H22V11H13V9M13,5H22V7H13V5M7.5,8A2.5,2.5 0 0,1 10,10.5A2.5,2.5 0 0,1 7.5,13A2.5,2.5 0 0,1 5,10.5A2.5,2.5 0 0,1 7.5,8Z" />
+                        <path fill="#000000" d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
                         </SvgIcon>
                       </ListItemIcon>
                       <ListItemText primary = {text} />
                     </ListItem>                                                         
                   ) :
                   index === 4 ? (                    
-
                     <ListItem button key = {text} onClick = {this.logOut}>
                       <ListItemIcon>
                         <SvgIcon>
